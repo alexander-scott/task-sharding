a9e55b64d9a8f80d27c000a5fefbf6002b802ab3
Alexander Scott <alexanderscott@outlook.com>
2021-12-27 22:56:09 +0000

    Create basic test for client

--- client/test/test_client.py
+++ client/test/test_client.py
@@ -1,8 +1,90 @@
 from src.client import Client
+from src.logger import Logger
 
+import json
+import queue
+import threading
 import unittest
 
 
+class MockConnection:
+    def __init__(self):
+        self.sent_messages = queue.Queue()
+        self.received_messages = queue.Queue()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        pass
+
+    def send_message(self, msg):
+        self.sent_messages.put(msg)
+
+    def get_sent_msg(self):
+        return self.sent_messages.get(block=True)
+
+    def close_websocket(self):
+        pass
+
+    def get_latest_message(self, timeout: float) -> dict:
+        return self.received_messages.get(block=False, timeout=timeout)
+
+
+class MockConfiguration:
+    def __init__(self, client_id, schema_path, workspace_path):
+        self.client_id = client_id
+        self.schema_path = schema_path
+        self.workspace_path = workspace_path
+
+
 class TestClient(unittest.TestCase):
-    def test_test1(self):
-        self.assertEqual(True, False)
+    def setUp(self):
+        self.logger = Logger("1")
+
+    def test_test(self):
+        config = MockConfiguration("1", "./client/test/test_schema.yaml", "test")
+        with MockConnection() as connection:
+            client = Client(config, connection, self.logger)
+            client_thread = threading.Thread(target=client.run)
+            client_thread.start()
+            connection.received_messages.put(
+                json.dumps(
+                    {
+                        "payload": {
+                            "consumer_id": "UUID1",
+                            "message_type": 2,
+                            "branch": "master",
+                            "cache_id": "1",
+                            "schema_id": "1",
+                            "step_id": "0",
+                        }
+                    }
+                )
+            )
+            msg1 = connection.get_sent_msg()
+            connection.received_messages.put(
+                json.dumps(
+                    {
+                        "payload": {
+                            "consumer_id": "UUID1",
+                            "message_type": 4,
+                            "branch": "master",
+                            "cache_id": "1",
+                            "schema_id": "1",
+                            "step_id": "0",
+                        }
+                    }
+                )
+            )
+            client_thread.join()
+            self.assertDictEqual(
+                {
+                    "message_type": 1,
+                    "branch": "master",
+                    "cache_id": "1",
+                    "schema_id": "sleep",
+                    "total_steps": 4,
+                },
+                msg1,
+            )
--- server/task_director/tests.py
+++ server/task_director/tests.py
@@ -45,11 +45,18 @@ class TaskDirectorTests__Setup(TestCase):
 
 
 class TaskDirectorTests__Messaging(TestCase):
-    def test__send_message(self):
-        controller = TaskDirectorController()
-        created_consumer = MockAsyncJsonWebsocketConsumer("UUID1")
-        controller.register_consumer("UUID1", created_consumer)
+    def setUp(self):
+        self.controller = TaskDirectorController()
+        self.consumer_id = "UUID1"
+        self.consumer = MockAsyncJsonWebsocketConsumer(self.consumer_id)
+        self.controller.register_consumer("UUID1", self.consumer)
 
+    def test__send_init_message(self):
+        """
+        GIVEN a freshly instantiated TaskDirectorController.
+        WHEN a consumer connects and sends an INIT message.
+        EXPECT the server to return to the same consumer a BUILD instruction message.
+        """
         mock_client_init_msg = {
             "message_type": MessageType.INIT,
             "branch": "master",
@@ -68,8 +75,8 @@ class TaskDirectorTests__Messaging(TestCase):
             }
         }
 
-        asyncio.get_event_loop().run_until_complete(controller.handle_received(mock_client_init_msg, "UUID1"))
+        asyncio.get_event_loop().run_until_complete(self.controller.handle_received(mock_client_init_msg, "UUID1"))
 
-        actual_server_build_msg = json.loads(created_consumer.get_sent_data())
+        actual_server_build_msg = json.loads(self.consumer.get_sent_data())
 
         self.assertDictEqual(expected_server_build_msg, actual_server_build_msg)
