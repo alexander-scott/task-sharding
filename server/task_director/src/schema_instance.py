cde21a0091aad2ace82547a1829f61e8b9fde6cf
Alexander Scott <alexanderscott@outlook.com>
2022-01-06 15:42:40 +0000

    [server] Add test for 3 instances

--- server/task_director/src/schema_instance.py
+++ server/task_director/src/schema_instance.py
@@ -29,7 +29,8 @@ class SchemaInstance:
         self._repo_states[consumer_id] = repo_state
 
     def deregister_consumer(self, consumer_id: str):
-        self._registered_consumers.remove(consumer_id)
+        if consumer_id in self._registered_consumers:
+            self._registered_consumers.remove(consumer_id)
         if consumer_id in self._in_progress_consumers:
             step_id = self._in_progress_consumers[consumer_id]
             del self._in_progress_consumers[consumer_id]
