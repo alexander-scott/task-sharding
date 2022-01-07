def8ad6b789d4eaac30cb3e4bde498dc96be837f
Alexander Scott <alexanderscott@outlook.com>
2022-01-07 21:06:09 +0000

    [client] Update bazel example shell script

--- examples/bazel/run-example.sh
+++ examples/bazel/run-example.sh
@@ -16,7 +16,7 @@ source venv/bin/activate
 pip3 install -r ../../server/requirements.txt
 pip3 install ../../client/dist/task_sharding_client_alexander_scott-0.0.1-py3-none-any.whl
 
-python3 client/main.py --client_id=1 --cache_id=1 --schema_path=./examples/bazel/schema.yaml --repo_state_path=. --workspace_path=./examples/bazel
+python3 main.py --client_id=1 --cache_id=1 --schema_path=schema.yaml --repo_state_path=. --workspace_path=.
 
 ret_code=$?
 
