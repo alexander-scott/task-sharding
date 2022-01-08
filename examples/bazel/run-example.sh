2627ae4de3b7476d014169e2e2dadb236701a79f
Alexander Scott <alexanderscott@outlook.com>
2022-01-08 11:59:36 +0000

    [examples] Move bazel example workspace into subfolder

--- examples/bazel/run-example.sh
+++ examples/bazel/run-example.sh
@@ -16,7 +16,7 @@ source venv/bin/activate
 pip3 install -r ../../server/requirements.txt
 pip3 install ../../client/dist/task_sharding_client_alexander_scott-0.0.1-py3-none-any.whl
 
-python3 main.py --client_id=1 --cache_id=1 --schema_path=schema.yaml --repo_state_path=. --workspace_path=.
+python3 main.py --client_id=1 --cache_id=1 --schema_path=schema.yaml --repo_state_path=. --workspace_path=ws
 
 ret_code=$?
 
